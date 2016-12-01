b74b3d2fef566cb97b595606370f4f142c4ea784
chinkyu <bamtori@hotmail.com>
2016-12-02 00:09:37 +0900

    add directory

---
+++ MyAndroidExcercise/ServiceTest3/app/src/main/java/com/bach/bamtori/servicetest3/MessengerService.java
@@ -0,0 +1,42 @@
+package com.bach.bamtori.servicetest3;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.Toast;
+
+/**
+ * Created by bamtori on 16. 11. 29.
+ */
+
+public class MessengerService extends Service {
+    private final static String TAG = MessengerService.class.getSimpleName();
+    static final int MSG_SAY_HELLO = 1;
+
+    // handler for incoming message
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case MSG_SAY_HELLO:
+                    Toast.makeText(getApplicationContext(),"hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    // Messenger.....
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_LONG).show();
+        return mMessenger.getBinder();
+    }
+}
---
+++ MyAndroidExcercise/ServiceTest3/app/src/main/java/com/bach/bamtori/servicetest3/MyServiceMainActivity.java
@@ -0,0 +1,122 @@
+package com.bach.bamtori.servicetest3;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyServiceMainActivity extends AppCompatActivity {
+    private final static String TAG = MyServiceMainActivity.class.getSimpleName();
+
+    // Messenger for communicating with the Service
+    Messenger mService = null;
+    boolean mBound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_service_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_service_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // bind Service
+        Intent intent = new Intent(this, MessengerService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    public void sayHello(View v) {
+        if(!mBound) return;
+
+        Message msg = Message.obtain(null, MessengerService.MSG_SAY_HELLO, 0, 0);
+        try {
+            mService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onButton1Click(View view) {
+        Log.d(TAG, "onButton1Click");
+
+        sayHello(view);
+    }
+
+    public void onButton2Click(View view) {
+        Log.d(TAG, "onButton2Click");
+    }
+
+    // ServiceConnection
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mService = new Messenger(iBinder);
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mService = null;
+            mBound = false;
+        }
+    };
+
+}
